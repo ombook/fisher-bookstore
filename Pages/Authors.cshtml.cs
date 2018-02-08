4c3539f6a4acf428792e46e7e01d2a379e83e933
Olivia Book <olmabook@outlook.com>
2018-02-08 16:19:41 -0500

    Added a razor-model branch

---
+++ Pages/Authors.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+namespace Fisher.Bookstore.Pages
+{
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Pages/Books.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+namespace Fisher.Bookstore.Pages
+{
+    public class Book
+    {    
+        public string Title { get; set; }
+
+        public Author Author { get; set; }
+    }
+    
+    public class Books : PageModel
+    {
+        public List<Book> books;
+
+        public void OnGet()
+        {
+            books = new List<Book>();
+
+            books.Add(new Book()
+            {
+                Title = "Continuous Delivery",
+                Author = new Author() { Name = "Jez Humble"}
+            });
+
+            books.Add(new Book()
+            {
+                Title = "Lean Enterprise",
+                Author = new Author() { Name = "Barry O'Reilly"}
+            });
+
+            books.Add(new Book()
+            {
+                Title = "The Goal",
+                Author = new Author() { Name = "Eliyahu Goldratt"}
+            });
+        }
+    }
+}
